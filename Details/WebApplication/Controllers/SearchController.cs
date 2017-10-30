d420effef89548da4741432c254a3a72abf11b94
Kirill Maloyaroslavtsev <k.maloyaroslavtsev@pflb.ru>
2017-10-30 11:20:14 +0300

    initial commit of backend solution

---
+++ Details/WebApplication/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication.Repositories.Interfaces;
+
+namespace WebApplication.Controllers
+{
+    [Route("api/[controller]")] 
+    public class SearchController : Controller
+    {
+        private readonly IDetailRepository _detailRepository;
+
+        public SearchController(IDetailRepository repo)
+        {
+            _detailRepository = repo;
+        }
+        
+        [HttpGet("quick/{q}")] // если без / в начале - то адресс относительно адреса контроллера
+        public JsonResult QuickSearch(string q)
+        {
+            var details=_detailRepository.GetDetails();
+            return Json(details);
+        }
+    }
+}
---
+++ Details/WebApplication/Repositories/Entities/DetailRepository.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using Dapper;
+using Npgsql;
+using WebApplication.DatabaseEntities;
+using WebApplication.Repositories.Interfaces;
+
+namespace WebApplication.Repositories.Entities
+{
+    public class DetailRepository : IDetailRepository
+    {
+        private readonly string _connectionString;
+
+        public DetailRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public void Create(Detail detail)
+        {
+            using (IDbConnection db = new NpgsqlConnection(_connectionString))
+            {
+                var sqlQuery = "INSERT INTO Detail (Type, DetailNumber,OriginalDetailNumber," +
+                               "Name,Price,Delivery,Description,StockQuantity) " +
+                               "VALUES(@Type, @DetailNumber,@OriginalDetailNumber," +
+                               "@Name,@Price,@Delivery,@Description,@StockQuantity); " +
+                               "SELECT CAST(SCOPE_IDENTITY() as int)"; // для получения ID созданного объекта
+                int? internalId = db.Query<int>(sqlQuery, detail).FirstOrDefault();
+                detail.InternalId = internalId.Value;
+            }
+        }
+
+        public void Delete(int internalId)
+        {
+            using (IDbConnection db = new NpgsqlConnection(_connectionString))
+            {
+                var sqlQuery = "DELETE FROM Detail WHERE internalId = @internalId";
+                db.Execute(sqlQuery, new {internalId});
+            }
+        }
+
+        public Detail Get(int internalId)
+        {
+            using (IDbConnection db = new NpgsqlConnection(_connectionString))
+            {
+                return db.Query<Detail>("SELECT * FROM Detail WHERE internalId = @internalId", new {internalId})
+                    .FirstOrDefault();
+            }
+        }
+
+        public List<Detail> GetDetails()
+        {
+            using (IDbConnection db = new NpgsqlConnection(_connectionString))
+            {
+                return db.Query<Detail>("SELECT * FROM Detail").ToList();
+            }
+        }
+
+        public void Update(Detail detail)
+        {
+            using (IDbConnection db = new NpgsqlConnection(_connectionString))
+            {
+                var sqlQuery = "UPDATE Detail SET Type = @Type, DetailNumber = @DetailNumber," +
+                               "OriginalDetailNumber=@OriginalDetailNumber," +
+                               "Name=@Name, Price=@Price, Delivery=@Delivery," +
+                               "Description=@Description, StockQuantity=@StockQuantity " +
+                               "WHERE InternalId = @InternalId";
+                db.Execute(sqlQuery, detail);
+            }
+        }
+    }
+}
