0a0a3bcdb0eb1375fb3d2eaed00e97375b2d877d
Kirill Maloyaroslavtsev <k.maloyaroslavtsev@pflb.ru>
2017-12-03 21:30:31 +0300

    Try 6

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Stopping running instance and delete old version of application"
-apt-get install sshpass
 sshpass -p '$KOLOBOKSPASSWORD' ssh koloboks@185.159.130.85 pkill -f 'dotnet' | pkill -f 'dotnet' | rm -rf /usr/details/details-output
+echo "Deploying..."
 sshpass -p '$KOLOBOKSPASSWORD' scp -r ./details-output/ koloboks@185.159.130.85:/usr/details/
+echo "Starting...."
 sshpass -p '$KOLOBOKSPASSWORD' ssh koloboks@185.159.130.85 dotnet /usr/details/details-output/WebApplication.dll &
